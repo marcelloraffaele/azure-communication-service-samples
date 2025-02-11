00eb57df9a57ecf6671be0dd93ec8733697bd198
Raffaele Marcello <rmarcello@microsoft.com>
2025-02-11 17:35:25 +0100

    Add join Teams meeting functionality with project setup and configuration

---
+++ 4.join-teams-meeting/index.js
@@ -0,0 +1,47 @@
+import { CallClient } from "@azure/communication-calling";
+import { Features } from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+
+let call;
+let callAgent;
+const meetingLinkInput = document.getElementById('teams-link-input');
+const hangUpButton = document.getElementById('hang-up-button');
+const teamsMeetingJoinButton = document.getElementById('join-meeting-button');
+const callStateElement = document.getElementById('call-state');
+const recordingStateElement = document.getElementById('recording-state');
+
+let myToken = "<Your ACS token>";
+
+async function init() {
+    const callClient = new CallClient();
+    const tokenCredential = new AzureCommunicationTokenCredential(myToken);
+    callAgent = await callClient.createCallAgent(tokenCredential, { displayName: 'ACS tester user' });
+    teamsMeetingJoinButton.disabled = false;
+}
+init();
+
+hangUpButton.addEventListener("click", async () => {
+    await call.hangUp();
+    hangUpButton.disabled = true;
+    teamsMeetingJoinButton.disabled = false;
+    callStateElement.innerText = '-';
+});
+
+teamsMeetingJoinButton.addEventListener("click", () => {
+    call = callAgent.join({ meetingLink: meetingLinkInput.value }, {});
+
+    call.on('stateChanged', () => {
+        callStateElement.innerText = call.state;
+    })
+
+    call.feature(Features.Recording).on('isRecordingActiveChanged', () => {
+        if (call.feature(Features.Recording).isRecordingActive) {
+            recordingStateElement.innerText = "This call is being recorded";
+        }
+        else {
+            recordingStateElement.innerText = "";
+        }
+    });
+    hangUpButton.disabled = false;
+    teamsMeetingJoinButton.disabled = true;
+});
